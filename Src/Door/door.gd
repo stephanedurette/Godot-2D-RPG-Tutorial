52b6d04517d6be2a2ea578f24dab458ffffc62e4
stephanedurette <stephanedurette@rogers.com>
2026-04-16 19:20:22 -0400

    no message

---
+++ Src/Door/door.gd
@@ -0,0 +1,25 @@
+class_name Door
+
+extends StaticBody2D
+
+@export var open_close_time: float
+
+@onready var collider: CollisionShape2D = $CollisionShape2D
+@onready var sprite: Sprite2D = $Sprite2D
+
+var tween: Tween
+
+func open():
+	_open_door.call_deferred(true)
+	
+func close():
+	_open_door.call_deferred(false)
+
+func _open_door(open: bool):
+	if tween != null:
+		tween.kill()
+	
+	collider.disabled = open
+	var final_modulate: Color = Color.TRANSPARENT if open else Color.WHITE
+	tween = get_tree().create_tween()
+	tween.tween_property(sprite, "self_modulate", final_modulate, open_close_time).set_ease(Tween.EASE_IN_OUT)

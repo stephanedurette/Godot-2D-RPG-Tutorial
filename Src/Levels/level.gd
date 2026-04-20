66ae463dbc2fe16128a497258c2e3d063b04f624
stephanedurette <stephanedurette@rogers.com>
2026-04-20 04:02:04 -0400

    y sort fixed

--- Src/Levels/level.gd
+++ Src/Levels/level.gd
@@ -6,11 +6,10 @@ extends Node2D
 @export var level_camera: Camera2D
 		
 func add_player(p: Player, positionNode: Node2D):
-	p.reparent(positionNode.get_parent())
 	p.global_position = positionNode.global_position
 	level_camera.owner = null
 	level_camera.reparent(p, false)
 		
 func remove_player():
-	level_camera.owner = self
+	#level_camera.owner = self
 	level_camera.reparent(self, false)

497db021f40230b40d4b73fdbfcd992784b0b019
stephanedurette <stephanedurette@rogers.com>
2026-04-22 10:25:55 -0400

    next is switch puzzles

--- Src/Levels/Dungeon Level/Dungeon Room Hider/room_hider.gd
+++ Src/Levels/Dungeon Level/Dungeon Room Hider/room_hider.gd
@@ -5,8 +5,14 @@ extends Node2D
 @export var collision_tilemap: TileMapLayer
 
 func _ready() -> void:
-	show_room(room_showing)
+	_toogle_room_visible(room_showing)
 
-func show_room(s: bool):
-	collision_tilemap.collision_enabled = !s
-	visible = !s
+func _toogle_room_visible(is_room_showing: bool):
+	collision_tilemap.collision_enabled = !is_room_showing
+	visible = !is_room_showing
+
+func show_room():
+	_toogle_room_visible(true)
+	
+func hide_room():
+	_toogle_room_visible(false)

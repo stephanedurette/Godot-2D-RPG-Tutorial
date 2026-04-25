c6d3b176daf925dfbf8f9ca4cd30f0901bc9c3f8
stephanedurette <stephanedurette@rogers.com>
2026-04-25 11:12:43 -0400

    no message

--- Src/BlockPuzzle/Buttons/push_button.gd
+++ Src/BlockPuzzle/Buttons/push_button.gd
@@ -9,18 +9,18 @@ signal unpressed
 
 @export var stays_pressed: bool = false
 
-var colliding_bodies: Dictionary[Node2D,bool]
+var colliding_bodies = 0
 
-func _on_body_entered(body: Node2D) -> void:
-		colliding_bodies.get_or_add(body, true)
+func _on_body_entered(_body: Node2D) -> void:
+		colliding_bodies +=1
 		sprite.play("pressed")
 		pressed.emit()
 
-func _on_body_exited(body: Node2D) -> void:
+func _on_body_exited(_body: Node2D) -> void:
 	if stays_pressed:
 		return
 	
-	colliding_bodies.erase(body)
-	if colliding_bodies.size() == 0:
+	colliding_bodies -= 1
+	if colliding_bodies == 0:
 		sprite.play("unpressed")
 		unpressed.emit()

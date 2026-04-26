3f6ccfa24f1a3d1fcb241d3ff1835ea556edc285
stephanedurette <stephanedurette@rogers.com>
2026-04-26 09:00:02 -0400

    no message

--- Src/Inventory/item_ui.gd
+++ Src/Inventory/item_ui.gd
@@ -2,8 +2,8 @@ class_name ItemUI
 
 extends Control
 
-@onready var image_texture: TextureRect = $ColorRect/Image
-@onready var count_text: Label = $ColorRect/Count
+@onready var image_texture: TextureRect = $HBoxContainer/Image
+@onready var count_text: Label = $HBoxContainer/Count
 
 func set_display(item: ItemData, count: int):
 	image_texture.texture = item.image
